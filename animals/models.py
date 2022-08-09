b96c2d57669e9f680afa1b9c3e5a607cb3ef79db
Richard Cândido <rich_dacan@protonmail.com>
2022-08-09 19:40:09 -0300

    feat :sparkles: created basic migrations and relations

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SexAnimals(models.TextChoices):
+    MACHO = "Macho"
+    FÊMEA = "Fêmea"
+    NOT_INFORMED = "Não Informado"
+
+
+class Animal(models.Model):
+    animal = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=15, choices=SexAnimals.choices, default=SexAnimals.NOT_INFORMED
+    )
--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
+from unicodedata import name
+
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    scientific_name = models.CharField(max_length=50, unique=True)
